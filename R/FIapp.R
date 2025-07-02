95a02b4fdc6583901dddaabea21f6cadbf65e25c
MarcoPittarello <marco.pittarello1987@yahoo.it>
2025-07-02 08:44:12 +0200

    aggiunta codice per caricamento e o installazione pacchetti fondamentali

--- R/FIapp.R
+++ R/FIapp.R
@@ -1,6 +1,17 @@
 #' Avvia la Shiny app di FaciesIdentificator
 #' @export
 FIapp <- function() {
+  
+  lib <- c ("vegan","shiny","shinythemes","writexl","DT","readr","readxl")
+  
+  # Install and load libraries
+  for (i in lib) {
+    if (i %in% installed.packages () [,"Package"] == F) {
+      install.packages (i)
+    }
+    library (i, character.only = TRUE)
+  }
+  
   appDir <- system.file("shiny/faciesapp", package = "FaciesIdentificator")
   if (appDir == "") {
     stop("App directory non trovata. Verifica la struttura del pacchetto.")
